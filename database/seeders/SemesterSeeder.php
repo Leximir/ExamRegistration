fd358b47b56010a5fe31d2436c6a320994b85aea
Leximir <aleksa.stankovic@proton.me>
2025-05-12 20:57:09 +0200

    Filled SemesterSeeder.php with real data.

--- database/seeders/SemesterSeeder.php
+++ database/seeders/SemesterSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SemesterSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class SemesterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('semesters')->insert([
+            ['semester' => 1 , 'year' => 1],
+            ['semester' => 2 , 'year' => 1],
+            ['semester' => 3 , 'year' => 2],
+            ['semester' => 4 , 'year' => 2],
+            ['semester' => 5 , 'year' => 3],
+            ['semester' => 6 , 'year' => 3],
+            ['semester' => 7 , 'year' => 4],
+            ['semester' => 8 , 'year' => 4],
+            ['semester' => 9 , 'year' => 5],
+            ['semester' => 10 , 'year' => 5],
+        ]);
     }
 }
