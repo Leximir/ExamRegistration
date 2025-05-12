b7be8b2ddd2949425fe2f376159f2a3840f001f4
Leximir <aleksa.stankovic@proton.me>
2025-05-12 21:00:19 +0200

    Filled ProfessorSeeder.php with some data for testing.

--- database/seeders/ProfessorSeeder.php
+++ database/seeders/ProfessorSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProfessorSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class ProfessorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('professors')->insert([
+            ['name' => 'Mirko Sajic'],
+            ['name' => 'Mladen Bubonjic']
+        ]);
     }
 }
