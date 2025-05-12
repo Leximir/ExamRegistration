9ba6ba9569b3b897d5c98e69cb1f5ccd9839d8a0
Leximir <aleksa.stankovic@proton.me>
2025-05-12 20:25:49 +0200

    Filled Subfaculty model with basic stuff.

--- app/Models/Subfaculty.php
+++ app/Models/Subfaculty.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subfaculty extends Model
 {
-    //
+    protected $table = 'subfaculties';
+    protected $fillable = ['name', 'faculty'];
+
+    public const FACULTIES = [
+        'ekonomija' => 'Ekonomski fakultet',
+        'pedagogija' => 'Pedagoški fakultet',
+        'politika' => 'Fakultet za političke nauke',
+        'ekologija' => 'Fakultet za ekologiju',
+        'informatika' => 'Fakultet za informatiku',
+        'umjetnost' => 'Fakultet lijepih umjetnosti',
+        'bezbjednost' => 'Fakultet za bezbjednost i zaštitu'
+    ];
 }
